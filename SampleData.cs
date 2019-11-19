4e62c3409c062380d062897a7b8dc6036ea2e768
Alex0ps <56863906+Alex0ps@users.noreply.github.com>
2019-11-19 12:50:32 +0300

    Initial commit

---
+++ SampleData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TVStore.Models;
+
+namespace TVStore
+{
+    public static class SampleData
+    {
+        public static void Initialize(TVContext context)
+        {
+            if (!context.TVs.Any())
+            {
+                context.TVs.AddRange(
+                    new TV
+                    {
+                        Name = "Apple iTV",
+                        Company = "Apple",
+                        Price = 10000
+                    },
+                    new TV
+                    {
+                        Name = "Bravia 4HDD3",
+                        Company = "Sony",
+                        Price = 700
+                    },
+                    new TV
+                    {
+                        Name = "32-UHD",
+                        Company = "Vivibright",
+                        Price = 450
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TVStore.Models;   
+using Microsoft.EntityFrameworkCore;
+
+namespace TVStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TVContext>(options => options.UseSqlServer(connection));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
